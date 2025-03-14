768044f1d98db1d9217943ae33c777d0619abdf9
sh1raf <mr.kir05@mail.ru>
2025-03-14 09:10:50 +0700

    Vowels function

---
+++ Vowels.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+string Vowels = "aeuoi";
+
+int GetVowelsAmount(string str)
+{
+	int amount = 0;
+	for(int i = 0; i < static_cast<int>(str.size)); i++)
+	{
+		for(int j = 0; j < static_cast<int>(Vowels.size)); j++)
+		{
+			if(str[i] == Vowels[j])
+			{
+				amount++;
+				break;
+			}
+		}
+	}
+
+	return amount;
+}
+
+int main()
+{
+	string str = "";
+	cin >> str;
+
+	cout << GetVowels(str);
+}
